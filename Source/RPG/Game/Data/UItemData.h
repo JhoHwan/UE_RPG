70c027506922e163182d7479daac98d3283ae161
oTangEo <90256741+JhoHwan@users.noreply.github.com>
2025-11-19 00:11:58 +0900

    Refactor character assets and add item data classes
    Reorganized and renamed character-related assets, removing old files and adding new assets for pants, body, and cloth. Introduced new C++ classes UItemData and UMP2AssetManager for item data and asset management. Updated map and blueprint assets to reflect these changes.

---
+++ Source/RPG/Game/Data/UItemData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "UItemData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UUItemData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RPG/Game/UMP2AssetManager.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "UMP2AssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RPG_API UUMP2AssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+};
